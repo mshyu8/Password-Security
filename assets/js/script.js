b2637ba9b670a5a7bbb75b5d58a09c718853773a
Michael Shyu <michaelshyu11@gmail.com>
2022-11-04 01:23:42 -0400

    changed the javascript using charAt to fix, and returned password outside for loop to get desired output

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,140 +17,134 @@ passwordText.value = password;
 var generatePassword = function() {
   
   var passwordLength = window.prompt("How long do you want your password to be? It must be inbetween 8 and 128 charcters.")
-  var lowerCase = window.prompt("Do you want lowercase letters in your password?");
-  var upperCase = window.prompt("Do you want uppercase letters in your password?");
-  var numericNumbers = window.prompt("Do you want numberic numbers in your password?");
-  var specialChars = window.prompt("Do you want special characters in your password?");
-
-
-/* var lowerCase = window.prompt("Do you want lowercase letters in your password?");
-  if (lowerCase === yes) {
-    localStorage.setItem("yes", lowerCase);
-  } else {
-    localStorage.setItem("no", lowerCase);
+  if (!passwordLength) {
+    return;
+  }
+  var lowerCase = window.prompt("Do you want lowercase letters in your password? (yes or no)");
+  if (!lowerCase) {
+    return;
+  }
+  var upperCase = window.prompt("Do you want uppercase letters in your password? (yes or no)");
+  if (!upperCase) {
+    return;
+  }
+  var numericNumbers = window.prompt("Do you want numberic numbers in your password? (yes or no)");
+  if (!numericNumbers) {
+    return;
+  }
+  var specialChars = window.prompt("Do you want special characters in your password? (yes or no)");
+  if (!specialChars) {
+    return;
   }
 
-  var upperCase = window.prompt("Do you want uppercase letters in your password?");
-   if (upperCase === yes) {
-      localStorage.setItem("yes", upperCase);
-    } else {
-      localStorage.setItem("no", upperCase);
-    }
-    
-    var numericNumbers = window.prompt("Do you want numberic numbers in your password?");
-      if (numericNumbers === yes) {
-        localStorage.setItem("yes", numericNumbers);
-      } else {
-        localStorage.setItem("no", specialChars);
-      }
-      
-      
-      var specialChars = window.prompt("Do you want special characters in your password?");
-        if (specialChars === yes) {
-          localStorage.setItem("yes", specialChars);
-        } else {
-          localStorage.setItem("no", specialChars);
-        }
-
-        var lower = "abcdefghijklmnopqrstuvwxyz";
-        var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        var numeric = "123456789";
-        var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-
-        */
 
         if (lowerCase === "yes" && upperCase === "yes" && specialChars === "yes" && numericNumbers === "yes") {
           var options = "abcdefghijklmnopqrstubwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-          for (i = 0; i <= passwordLength; i++) {
-          var randomNumber = Math.floor(Math.random() * options.length);
-          password += options.substring(randomNumber, randomNumber +1);
-          }
-         } else if (lowerCase === yes && upperCase === yes && specialChars === yes && numericNumbers === no) {
+          password = "";
+          for (i = 0; i < passwordLength; i++) {
+          password += options.charAt(Math.floor(Math.random() * options.length));      
+        }
+        return password;
+      } else if (lowerCase === "yes" && upperCase === "yes" && specialChars === "yes" && numericNumbers === "no") {
           var options = "abcdefghijklmnopqrstubwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          } 
-         } else if (lowerCase === yes && upperCase === yes && specialChars === no && numericNumbers === yes) {
+         return password;
+         } else if (lowerCase === "yes" && upperCase === "yes" && specialChars === "no" && numericNumbers === "yes") {
           var options = "abcdefghijklmnopqrstubwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          }
-        } else if (lowerCase === yes && upperCase === no && specialChars === yes && numericNumbers === yes) {
+         return password;
+        } else if (lowerCase === "yes" && upperCase === "no" && specialChars === "yes" && numericNumbers === "yes") {
           var options = "abcdefghijklmnopqrstubwxyz123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          }
-        } else if (lowerCase === no && upperCase === yes && specialChars === yes && numericNumbers === yes) {
+         return password;
+        } else if (lowerCase === "no" && upperCase === "yes" && specialChars === "yes" && numericNumbers === "yes") {
           var options = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          }
-        } else if (lowerCase === yes && upperCase === no && specialChars === no && numericNumbers === no) {
+         return password;
+        } else if (lowerCase === "yes" && upperCase === "no" && specialChars === "no" && numericNumbers === "no") {
           var options = "abcdefghijklmnopqrstubwxyz";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
           }
-         } else if (lowerCase === no && upperCase === yes && specialChars === no && numericNumbers === no) {
+          return password;
+         } else if (lowerCase === "no" && upperCase === "yes" && specialChars === "no" && numericNumbers === "no") {
           var options = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          }  
-        }  else if (lowerCase === no && upperCase === no && specialChars === yes && numericNumbers === no) {
+         return password;
+        }  else if (lowerCase === "no" && upperCase === "no" && specialChars === "yes" && numericNumbers === "no") {
           var options = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          }  
-        }  else if (lowerCase === no && upperCase === no && specialChars === no && numericNumbers === yes) {
+         return password;
+        }  else if (lowerCase === "no" && upperCase === "no" && specialChars === "no" && numericNumbers === "yes") {
           var options = "123456789";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          }  
-        }  else if (lowerCase === yes && upperCase === yes && specialChars === no && numericNumbers === no) {
+         return password;
+        }  else if (lowerCase === "yes" && upperCase === "yes" && specialChars === "no" && numericNumbers === "no") {
           var options = "abcdefghijklmnopqrstubwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          }  
-        }  else if (lowerCase === yes && upperCase === no && specialChars === yes && numericNumbers === no) {
+         return password;
+        }  else if (lowerCase === "yes" && upperCase === "no" && specialChars === "yes" && numericNumbers === "no") {
           var options = "abcdefghijklmnopqrstubwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          }  
-        }  else if (lowerCase === yes && upperCase === no && specialChars === no && numericNumbers === yes) {
+         return password;
+        }  else if (lowerCase === "yes" && upperCase === "no" && specialChars === "no" && numericNumbers === "yes") {
           var options = "abcdefghijklmnopqrstubwxyz123456789";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          }  
-        }  else if (lowerCase === no && upperCase === yes && specialChars === yes && numericNumbers === no) {
+         return password;
+        }  else if (lowerCase === "no" && upperCase === "yes" && specialChars === "yes" && numericNumbers === "no") {
           var options = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          }  
-        }  else if (lowerCase === no && upperCase === yes && specialChars === no && numericNumbers === yes) {
+         return password;
+        }  else if (lowerCase === "no" && upperCase === "yes" && specialChars === "no" && numericNumbers === "yes") {
           var options = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          }  
-        }  else if (lowerCase === no && upperCase === no && specialChars === yes && numericNumbers === yes) {
+         return password;
+        }  else if (lowerCase === "no" && upperCase === "no" && specialChars === "yes" && numericNumbers === "yes") {
           var options = "123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-          for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * options.length);
-            password += chars.substring(randomNumber, randomNumber +1);
+          password = "";
+          for (var i = 0; i < passwordLength; i++) {
+            password += options.charAt(Math.floor(Math.random() * options.length));
          }  
-        } else {
-          alert("You must choose one of the options.")
+         return password;
+        } else if (lowerCase === "no" && upperCase === "no" && specialChars === "no" && numericNumbers === "no") {
+          alert("You must choose one of the options.");
           generatePassword();
         }
 
